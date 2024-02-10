aeaf74a29f87f5ec83a8206231a9189f640984d6
hayat118 <tariqueht6@gmail.com>
2024-02-10 18:26:23 +0530

    add tailwind and styling

--- src/App.js
+++ src/App.js
@@ -7,10 +7,28 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Home />
-        {/* <Counter /> */}
-      </header>
+      {/* <Counter /> */}
+      <div className="grid grid-cols-12 gap-4">
+        <div className="col-span-3">
+          <div className="h-16 border-b border-gray-100 center align-middle ">
+            <h1>QuotesVerse</h1>
+          </div>
+        </div>
+
+        <div className="col-span-6">
+          <div>
+            <p>Home</p>
+            <p>Like</p>
+          </div>
+          <Home />
+        </div>
+
+        <div className="col-span-3">
+          <div>
+            <h2>Treding Topics</h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/features/home/home.js
+++ src/features/home/home.js
@@ -1,5 +1,8 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+
+import { HeartIcon } from "@heroicons/react/24/outline";
+
 import {
   quotesAdded,
   fetchQuotes,
@@ -22,17 +25,31 @@ const Home = () => {
 
   return (
     <div>
-      <div>
+      <div className="space-y-6">
         {quotes.map((quote, i) => {
           return (
-            <div>
-              <p>{quote.content}</p>
-              <span
-                style={{ textDecoration: "underline" }}
-                onClick={() => handleLikeQuote(quote._id)}
-              >
-                Like - {quote.isLiked ? "Liked" : "Unliked"}
-              </span>
+            <div className="grid grid-cols-12 border-b p-4 border-gray-700">
+              <div className="col-span-1">
+                <img src="/avatar.png" className="h-8 w-8 rounded-full" />
+              </div>
+
+              <div className="col-span-11 text-left space-y-2">
+                <h2 className="font-semibold">{quote.author}</h2>
+                <p>{quote.content}</p>
+                <HeartIcon
+                  className={
+                    quote.isLiked ? "h-6 w-6 text-red-800" : "h-6 w-6 "
+                  }
+                  onClick={() => handleLikeQuote(quote._id)}
+                />
+                <p>
+                  {quote.tags.map((tag, j) => (
+                    <span className="text-sm text-gray-500" key={j}>
+                      {tag}
+                    </span>
+                  ))}
+                </p>
+              </div>
             </div>
           );
         })}
